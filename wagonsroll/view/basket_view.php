9f8484ecb93fee51281fb0b01bf56eff571a846c
Nazar Mikhailovich <nazarii.mykhailovych.knm.2019@lpnu.ua>
2022-03-11 16:40:11 +0000

    Added homepage and basket views

---
+++ wagonsroll/view/basket_view.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Wagons Roll</title>
+</head>
+<body>
+
+<!--  -->
+<div>
+</div>
+
+<div>
+<a>Items</a>
+</div>
+<div>
+<a>Order</a>
+</div>
+<div>
+<a>Pay</a>
+</div>
+<br>
+
+
+<div class="carblock" style="border: 2px outset black; width:300px;">
+<img src="https://media.autoexpress.co.uk/image/private/s--wqUB0fzD--/f_auto,t_content-image-full-mobile@1/v1645649253/autoexpress/2022/02/BMW-X1_xf13cc.jpg" style="width: 100%;">
+<a>BMW X1 Aut.</a><br>
+<!-- missing onclick and .php for this one as of now -->
+<button>x</button><br>
+<a>183/day</a><br>
+<a>Start date:</a>
+<a>12.04.2021</a> <br>
+<a>End date:</a>
+<a>17.04.2021</a><br>
+<a>Total:</a>
+<a> 732</a>
+</div>
+
+<br>
+<div style="border: 2px outset black; width:300px;">
+<a>Total:</a>
+<a>1464</a>
+</div>
+
+<br>
+<button> Next </button>
+
+
+</body>
+</html>
+
