e79a1a7c044af70675090662964583742c267003
Sofiia Popadiuk <popadiuk.sophiya@gmail.com>
2022-03-19 17:58:40 +0200

    Update location of the new vehicles block on the user home page

--- wagonsroll/view/home_view.php
+++ wagonsroll/view/home_view.php
@@ -19,34 +19,10 @@
     </head>
     <body>
         <div class="container-fluid">
-            <h2 style="text-align:center">New vehicles:</h2>
-            <div style="display:flex; justify-content:center">
-                <div style="position:relative" id="carouselExampleDark" class="carousel carousel-dark slide w-75" data-bs-ride="carousel" style="margin-bottom:30px">
-                    <div class="carousel-indicators" style="position:absolute; bottom:100px">
-                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                    </div>
-                    <div class="carousel-inner"></div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-
-            <hr>
-
-            <?php require_once "block/directSearch.php" ?>
             <br>
             <?php require_once "block/directSearch.php" ?>
             <br>
+            <?php require_once "block/directSearch.php" ?>
             <br>
 
             <?php require_once "block/filterDisplay.php" ?>
@@ -66,6 +42,28 @@
                 <input type="date" id="enddate" name="enddate"><br>
                 <input type="submit" class="btn btn-secondary mt-1" value="Book">
             </form>
+
+            <h2 style="text-align:center">New vehicles:</h2>
+            <div style="display:flex; justify-content:center; margin-bottom:10px">
+                <div style="position:relative" id="carouselExampleDark" class="carousel carousel-dark slide w-75" data-bs-ride="carousel" style="margin-bottom:30px">
+                    <div class="carousel-indicators" style="position:absolute; bottom:100px">
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                    </div>
+                    <div class="carousel-inner"></div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
         </div>
     </body>
 </html>
